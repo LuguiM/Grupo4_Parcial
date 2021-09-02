90c9ce8daaee4cdce38d0e66b5ec641bcabf8a61
JOSÉ LUIS MUÑOZ PADILLA <USIS043721@ugb.edu.sv>
2021-09-02 15:25:09 -0600

    mdi

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,10 +29,18 @@ namespace Semana6_Grupo4_Parcial
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.IsMdiContainer = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
